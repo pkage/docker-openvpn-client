b952d67724b5f6ae3c5249ab9f7e87b19be46a4a
Patrick Kage <patrick.r.kage@gmail.com>
2025-05-19 13:16:09 -0400

    remove script security

--- build/entry.sh
+++ build/entry.sh
@@ -37,9 +37,6 @@ if is_enabled "$KILL_SWITCH"; then
     openvpn_args+=("--route-up" "/usr/local/bin/killswitch.sh $ALLOWED_SUBNETS")
 fi
 
-# ensure that killswitch gets called
-openvpn_args+=("--script-security", "2")
-
 # # Docker secret that contains the credentials for accessing the VPN.
 # if [[ $AUTH_SECRET ]]; then
 #     openvpn_args+=("--auth-user-pass" "/run/secrets/$AUTH_SECRET")
